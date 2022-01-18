8b27a990e01bf4d42c382c63077e5d8012823d41
prasanna joshi <prasannajoshi3511@gmail.com>
2022-01-19 00:33:02 +0530

    latest

--- src/Home.js
+++ src/Home.js
@@ -26,7 +26,7 @@ function Home() {
      </div>
 
         <div className = "home_row" >
-        <h1 > Products </h1>
+        <h1 > Product</h1>
         
          <hr / >
         <Product id = "1"
