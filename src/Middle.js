b838c38bda7caf66073273a4eed7c1e0aeb77b21
prasanna joshi <prasannajoshi3511@gmail.com>
2022-01-18 23:55:15 +0530

    ddd

---
+++ src/Middle.js
@@ -0,0 +1,26 @@
+
+import React from 'react'
+import './Middle.css'
+
+export const Middle = () => {
+    return (
+        <div className='aboutf'>
+                
+        <div class="about">
+            <div class="inner-section">
+                <h1>About Us</h1>
+                <p class="text"><b>
+                    Origin Tech is a startup company . Started by Mr. Shubham Omase to make innovative highly reliable and easy to use , electronics & electrical products in different domains for e.g. IOT , healthcare , domestic , industrial , education , agriculture , etc.
+                    </b>
+                </p>
+                <div class="skills">
+                    <button>Contact Us</button>
+                </div>
+            </div>
+        </div>
+        <br/>
+
+        </div>
+    
+    )
+}
